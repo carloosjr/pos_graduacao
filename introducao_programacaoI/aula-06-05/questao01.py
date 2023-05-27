95d925bacaeff7c857a7a6a063c08abe4e4fbd2d
Carlos Jr <josecarlosoares7@gmail.com>
2023-05-26 21:21:11 -0300

    Adicionando diretório da aula do dia 06/05/23

---
+++ introducao_programacaoI/aula-06-05/questao01.py
@@ -0,0 +1,14 @@
+"""
+Crie um programa com uma mensagem pessoal que você ama programar em Python. Imprima essa mensagem em letras
+maiúsculas e depois em minúsculas.
+"""
+
+print("Eu amo programar em Python!")
+mensagem = "Eu amo programar em Python!"
+
+"""Imprimir em caixa alta"""
+print(mensagem.upper())
+
+"""Imprimir em caixa baixa"""
+print(mensagem.lower())
+
