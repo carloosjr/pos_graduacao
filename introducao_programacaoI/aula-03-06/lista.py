87bdb6b9e2264307d1de94185c9a1fcc6b83998e
carlosjr <josecarlosoares7@gmail.com>
2023-06-03 15:07:49 -0300

    Listas - Estruturas de controle

---
+++ introducao_programacaoI/aula-03-06/lista.py
@@ -0,0 +1,15 @@
+# Criando uma lista
+nomes = ['Jose','Carlos','Soares']
+
+print(nomes)
+
+# Imprimindo o tipo de dados
+print(f'O tipo do dado é: {type(nomes)}')
+
+# Imprimindo a posição pelo índice
+print(f'Imprimindo a posição 0-> {nomes[0]}')
+
+# Alterando o valor do índice
+nomes[1] = 'Júnior'
+
+print(nomes)
