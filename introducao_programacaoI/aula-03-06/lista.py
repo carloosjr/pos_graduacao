6b0684ea7993993d026431e8db4ab2ebcea7cde8
carlosjr <josecarlosoares7@gmail.com>
2023-06-03 15:25:33 -0300

    Atualista Lista.py

--- introducao_programacaoI/aula-03-06/lista.py
+++ introducao_programacaoI/aula-03-06/lista.py
@@ -11,5 +11,16 @@ print(f'Imprimindo a posição 0-> {nomes[0]}')
 
 # Alterando o valor do índice
 nomes[1] = 'Júnior'
-
 print(nomes)
+
+# Inserindo um valor na lista
+nomes.insert(1, 'Carlos')
+print(nomes)
+
+# Removendo um valor da lista
+del nomes[0]
+print(nomes)
+
+# Identificando índice do valor
+indice_carlos = nomes.index('Carlos')
+print(f'O indice da melancia é: {indice_carlos}')
