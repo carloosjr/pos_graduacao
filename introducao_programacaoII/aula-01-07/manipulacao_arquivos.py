b55a8bb86d87b485efe36cce46535b0b952a56b3
carlosjr <josecarlosoares7@gmail.com>
2023-07-09 21:02:33 -0300

    Delete introducao_programacaoII/aula-01-07 directory
    Corrigindo diretório

--- introducao_programacaoII/aula-01-07/manipulacao_arquivos.py
+++
@@ -1,5 +0,0 @@
-# Abrindo e lendo o arquivo em .txt
-
-with open('arquivo.txt','r', encoding='utf-8') as file_object:
-    conteudo = file_object.read()
-    print(f'Tipo: {type(conteudo)} \n{conteudo}')
